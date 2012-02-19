a378ec07498ff2a598e6017a1b5dcd460864e32f
robknows <robmoore121@hotmail.co.uk>
2012-02-19 21:39:33 +0000

    reformat

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,14 @@
 public class MainClass {
-	public static void main(String args[]){
-		TitleScreenCredits.titleRun();
-	}
-	public static void game(){
-		//System.out.println(PlayerAtt.name);
-		//MeleeBattle.meleeFight(Enemy.enemyYouFight, PlayerAtt.player);
-		CombatBoard.boardDisplay();
-		System.out.println("Program Terminated");
-	}
+    public static void main(String args[]) {
+        //TitleScreenCredits.titleRun();
+        game();
+    }
+
+    public static void game() {
+        //System.out.println(PlayerAtt.name);
+        //MeleeBattle.meleeFight(Enemy.enemyYouFight, PlayerAtt.player);
+        //CombatBoard.boardDisplay();
+        Screen.screen();
+        System.out.println("Program Terminated");
+    }
 }
---
+++ src/Screen.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Screen {
+
+    public static void screen() {
+        //game screen
+        JFrame frame = new JFrame("The Game");
+        frame.setVisible(true);
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //screen panel
+        JPanel panel = new JPanel(new GridBagLayout());
+        frame.getContentPane().add(panel, BorderLayout.CENTER);
+        GridBagConstraints c = new GridBagConstraints();
+
+        //title and button
+        JLabel gameName = new JLabel("The Game");
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(10, 10, 10, 10);
+        panel.add(gameName, c);
+        JButton startGame = new JButton("Start Game");
+        c.gridx = 0;
+        c.gridy = 2;
+        panel.add(startGame, c);
+        JButton instructions = new JButton("Instructions");
+        c.gridx = 0;
+        c.gridy = 3;
+        panel.add(instructions, c);
+        JButton credits = new JButton("Credits");
+        c.gridx = 0;
+        c.gridy = 4;
+        panel.add(credits, c);
+
+        //add panel to frame
+        frame.add(panel, c);
+    }
+}
