1d0e2f1de4f9a7a24a61f96849a79c1db013d7b1
robknows <robmoore121@hotmail.co.uk>
2012-02-23 16:58:41 +0000

    this code is not fully functional yet

--- src/rob/Board.java
+++ src/rob/Board.java
@@ -1,8 +1,11 @@
 package rob;
 
-public class Board {
+import java.util.Scanner;
 
-    public static void displayBoard(String[][] board) {
+public class Board {
+    static Scanner input = new Scanner(System.in);
+
+    private static void displayBoard(String[][] board) {
         for (String[] row : board) {
             for (int column = 0; column < row.length; column++) {
                 System.out.print(row[column]);
@@ -14,12 +17,61 @@ public class Board {
         }
     }
 
-    public static void setAll(String[][] board, String s) {
+    private static void setAll(String[][] board, String s) {
         for (int rowNumber = 0; rowNumber < board.length; rowNumber++) {
             String[] row = board[rowNumber];
             for (int columnNumber = 0; columnNumber < row.length; columnNumber++) {
                 board[columnNumber][rowNumber] = s;
             }
         }
+    }
+
+    public static void move(String[][] board, int x, int y, PlayerAtt player) {
+        setBoard();
+        directionPlayer(board, 2, 1, player);
+    }
+
+    private static void setBoard() {
+        String[][] board = new String[5][5];
+        Board.setAll(board, "O");
+        board[2][1] = "P";
+        board[2][3] = "E";
+        Board.displayBoard(board);
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void directionPlayer(String[][] board, int x, int y, PlayerAtt player) {
+        System.out.println("Right");
+        System.out.println("Left");
+        System.out.println("Down");
+        System.out.println("Up");
+        int choice = input.nextInt();
+        if (choice == 1) {
+            board[x][y] = "O";
+            board[x][y + howFar(player)] = "P";
+            Board.displayBoard(board);
+        } else if (choice == 2) {
+            board[x][y] = "O";
+            board[x][y - howFar(player)] = "P";
+            Board.displayBoard(board);
+        } else if (choice == 3) {
+            board[x][y] = "O";
+            board[x + howFar(player)][y] = "P";
+            Board.displayBoard(board);
+        } else if (choice == 4) {
+            board[x][y] = "O";
+            board[x - howFar(player)][y] = "P";
+            Board.displayBoard(board);
+        }
+    }
+
+    private static int howFar(PlayerAtt player) {
+        System.out.println("How far in that direction would you like to go?");
+        int choice = input.nextInt();
+        if (choice > player.pace) {
+            choice = 1;
+        }
+        return (choice);
     }
 }
--- src/rob/MainClass.java
+++ src/rob/MainClass.java
@@ -1,19 +1,24 @@
 package rob;
 
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String args[]) {
         //TitleScreenCredits.titleRun();
         game();
     }
 
+    static Scanner input = new Scanner(System.in);
+
     public static void game() {
         //System.out.println(rob.PlayerAtt.name);
         //CombatEngine.combatTurn(Enemy.enemyYouFight, PlayerAtt.player);
-        String[][] board = new String[7][7];
-        Board.setAll(board, "X");
-        board[3][1] = "P";
-        board[3][5] = "E";
-        Board.displayBoard(board);
+        String[][] board = new String[5][5];
+        CombatEngine.move(board, 2, 1, PlayerAtt.player);
         System.out.println("Program Terminated");
     }
+    //right = board[x][y+1]
+    //left = board[x][y-1]
+    //down = board[x+1][y]
+    //up = board[x-1][y]
 }
