c47630cd3f20c265191e8d09f5236853fa48f9d1
Robert Moore <robmoore121@hotmail.co.uk>
2012-02-25 22:36:16 +0000

    maze game patch

--- src/rob/mazeGame/Board.java
+++ src/rob/mazeGame/Board.java
@@ -121,10 +121,3 @@ public class Board {
         System.out.println("You Won!!!");
     }
 }
-//make a board
-//make it so the board is randomly generated
-//make the player start at the start
-//player wins if they reach the exit
-//player loses if they run our of time
-//make it so the boards size is based on the number of decisions the player must make
-//the decisions have a random number less than 5 of turns
--- src/rob/mazeGame/Player.java
+++ src/rob/mazeGame/Player.java
@@ -3,4 +3,4 @@ package rob.mazeGame;
 public class Player {
     static int xpos = 0;
     static int ypos = 0;
-}
+}//
