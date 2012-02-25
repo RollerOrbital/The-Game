688266da88e9bc7abf6a951c194bceaffe4697a3
Robert Moore <robmoore121@hotmail.co.uk>
2012-02-25 14:40:01 +0000

    update

--- src/rob/MainClass.java
+++ src/rob/MainClass.java
@@ -11,23 +11,4 @@ public class MainClass {
         CombatEngine.combatTurn(Enemy.enemyYouFight, PlayerAtt.player, Board.board);
         System.out.println("Program Terminated");
     }
-
-    //Bug 1
-
-//    What is the xco of where you want to go?
-//    1
-//    What is the yco of where you want to go?
-//    1
-//    You are now at position (1,1)
-//    O	O	O	O	O
-//    O	P	O	O	E	O	O	O	O	O	O
-//    O	O	O	O	O
-//    O	O	O	O	O
-
-    //what is THIS?!
-
-
-    //Bug 2
-
-//    The player can still go on top of the enemy without contest -__-
 }
