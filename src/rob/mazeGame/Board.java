82b7cde541667efd200a42f1b757a0094c7e563d
Robert Moore <robmoore121@hotmail.co.uk>
2012-02-25 18:37:44 +0000

    maze game project

---
+++ src/rob/mazeGame/Board.java
@@ -0,0 +1,11 @@
+package rob.mazeGame;
+
+public class Board {
+}
+//make a board
+//make it so the board is randomly generated
+//make the player start at the start
+//player wins if they reach the exit
+//player loses if they run our of time
+//make it so the boards size is based on the number of decisions the player must make
+//the decisions have a random number less than 5 of turns
---
+++ src/rob/mazeGame/Decision.java
@@ -0,0 +1,10 @@
+package rob.mazeGame;
+
+public class Decision {
+
+    public Decision() {
+
+    }
+}
+//decision has direction, number of paths and decision number
+//
