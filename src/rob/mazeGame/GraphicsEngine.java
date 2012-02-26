311d4807b12d1c4079fc0e259fa0ebe2aa46eef4
Robert Moore <robmoore121@hotmail.co.uk>
2012-02-26 09:18:28 +0000

    trying graphics...

--- src/rob/mazeGame/GraphicsEngine.java
+++ src/rob/mazeGame/GraphicsEngine.java
@@ -1,5 +1,16 @@
 package rob.mazeGame;
 
-public class GraphicsEngine {
+import javax.swing.*;
 
+public class GraphicsEngine {
+    public static void getFrame() {
+        JFrame frame = new JFrame("Maze Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setSize(274, 176);
+    }
+
+    public static void getPanel() {
+        JPanel panel = new JPanel();
+    }
 }
