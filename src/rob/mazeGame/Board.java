a4afcdd2bf4ab993449b258da1e22246a4008e38
Robert Moore <robmoore121@hotmail.co.uk>
2012-02-26 15:45:32 +0000

    sorry kaashif... I stole all of your already stolen code... ;)

--- src/rob/mazeGame/Board.java
+++ src/rob/mazeGame/Board.java
@@ -1,144 +1,195 @@
 package rob.mazeGame;
 
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class Board {
-    static Scanner input = new Scanner(System.in);
+public class Board extends JPanel implements ActionListener {
+    private Timer timer;
+    private Player player;
 
-    static String[][] board = new String[10][10];
+    public Board() {
 
-    public static void displayBoard(String[][] board) {
-        for (int x = 0; x < 10; x++) {
-            for (int i = 0; i < 10; i++) {
+        addKeyListener(new adapter());
+        setFocusable(true);
+        setBackground(Color.BLACK);
+        setDoubleBuffered(true);
 
-                board[x][i] = ".";
+        player = new Player();
 
-                board[0][1] = "W";
-                board[2][0] = "W";
-                board[2][1] = "W";
-                board[3][1] = "W";
-                board[2][3] = "W";
-                board[3][3] = "W";
-                board[1][3] = "W";
-                board[1][4] = "W";
-                board[5][3] = "W";
-                board[5][2] = "W";
-                board[3][4] = "W";
-                board[3][5] = "W";
-                board[5][1] = "W";
-                board[7][0] = "W";
-                board[7][1] = "W";
-                board[8][1] = "W";
-                board[7][3] = "W";
-                board[8][3] = "W";
-                board[8][4] = "W";
-                board[8][5] = "W";
-                board[8][6] = "W";
-                board[8][8] = "W";
-                board[9][8] = "W";
-                board[0][7] = "W";
-                board[1][7] = "W";
-                board[2][7] = "W";
-                board[1][6] = "W";
-                board[4][7] = "W";
-                board[5][7] = "W";
-                board[5][5] = "W";
-                board[6][5] = "W";
-                board[6][6] = "W";
-                board[7][8] = "W";
-                board[7][9] = "W";
-                board[3][9] = "W";
-                board[4][9] = "W";
-                board[5][8] = "W";
-                board[5][9] = "W";
-                board[2][9] = "W";
-                board[1][9] = "W";
+        timer = new Timer(5, this);
+        timer.start();
 
-                if (Player.xpos == 6 && Player.ypos == 9) {
-                    board[7][9] = ".";
-                }
+    }
 
-                try {
-                    if (!board[Player.xpos][Player.ypos].equals("W")) {
-                        board[Player.xpos][Player.ypos] = "P";
-                    } else if (Player.xpos == 7 && Player.ypos == 9) {
-                        board[Player.xpos][Player.ypos] = "P";
-                    } else {
-                        Player.xpos = 0;
-                        Player.ypos = 0;
-                        board[0][0] = "P";
-                        Player.fouls++;
-                    }
-                } catch (Exception e) {
-                    Player.xpos = 0;
-                    Player.ypos = 0;
-                    board[0][0] = "P";
-                    Player.fouls++;
-                }
-                board[9][9] = "F";
-                System.out.print(board[x][i] + "\t");
-                if (i == 9) {
-                    System.out.println();
-                }
-            }
+    public void paint(Graphics g) {
+        super.paint(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.drawImage(player.getImage(), player.getX(), player.getY(), this);
+
+        Toolkit.getDefaultToolkit().sync();
+        g.dispose();
+    }
+
+
+    public void actionPerformed(ActionEvent e) {
+        player.move();
+        repaint();
+    }
+
+
+    private class adapter extends KeyAdapter {
+
+        public void keyPressed(KeyEvent e) {
+            player.keyPressed(e);
         }
-    }
 
-    public static void playerMoves() {
-        switch (choice()) {
-            case 1:
-                Player.xpos++;
-                break;
-            case 2:
-                Player.xpos--;
-                break;
-            case 3:
-                Player.ypos++;
-                break;
-            case 4:
-                Player.ypos--;
-                break;
-        }
-    }
-
-    private static int choice() {
-        System.out.println("###");
-        String d = input.nextLine();
-        if (d.equals("s")) {
-            return 1;
-        } else if (d.equals("w")) {
-            return 2;
-        } else if (d.equals("d")) {
-            return 3;
-        } else if (d.equals("a")) {
-            return 4;
-        } else {
-            return 1;
-        }
-    }
-
-    public static boolean isGameOver() {
-        return Player.xpos == 9 && Player.ypos == 9;
-    }
-
-    public static void game() {
-        int numberOfMoves = 0;
-        while (!(isGameOver())) {
-            displayBoard(board);
-            playerMoves();
-            numberOfMoves++;
-        }
-        int score = lowerOne(100 - numberOfMoves - Player.fouls);
-        System.out.println("You Won!!!");
-        input.next();
-        System.out.println("Your score was " + score + "!");
-    }
-
-    private static int lowerOne(int x) {
-        if (x < 1) {
-            return (1);
-        } else {
-            return (x);
+        public void keyReleased(KeyEvent e) {
+            player.keyReleased(e);
         }
     }
 }
+
+
+//    static Scanner input = new Scanner(System.in);
+//
+//    static String[][] board = new String[10][10];
+//
+//    public static void displayBoard(String[][] board) {
+//        for (int x = 0; x < 10; x++) {
+//            for (int i = 0; i < 10; i++) {
+//
+//                board[x][i] = ".";
+//
+//                board[0][1] = "W";
+//                board[2][0] = "W";
+//                board[2][1] = "W";
+//                board[3][1] = "W";
+//                board[2][3] = "W";
+//                board[3][3] = "W";
+//                board[1][3] = "W";
+//                board[1][4] = "W";
+//                board[5][3] = "W";
+//                board[5][2] = "W";
+//                board[3][4] = "W";
+//                board[3][5] = "W";
+//                board[5][1] = "W";
+//                board[7][0] = "W";
+//                board[7][1] = "W";
+//                board[8][1] = "W";
+//                board[7][3] = "W";
+//                board[8][3] = "W";
+//                board[8][4] = "W";
+//                board[8][5] = "W";
+//                board[8][6] = "W";
+//                board[8][8] = "W";
+//                board[9][8] = "W";
+//                board[0][7] = "W";
+//                board[1][7] = "W";
+//                board[2][7] = "W";
+//                board[1][6] = "W";
+//                board[4][7] = "W";
+//                board[5][7] = "W";
+//                board[5][5] = "W";
+//                board[6][5] = "W";
+//                board[6][6] = "W";
+//                board[7][8] = "W";
+//                board[7][9] = "W";
+//                board[3][9] = "W";
+//                board[4][9] = "W";
+//                board[5][8] = "W";
+//                board[5][9] = "W";
+//                board[2][9] = "W";
+//                board[1][9] = "W";
+//
+//                if (Player.xpos == 6 && Player.ypos == 9) {
+//                    board[7][9] = ".";
+//                }
+//
+//                try {
+//                    if (!board[Player.xpos][Player.ypos].equals("W")) {
+//                        board[Player.xpos][Player.ypos] = "P";
+//                    } else if (Player.xpos == 7 && Player.ypos == 9) {
+//                        board[Player.xpos][Player.ypos] = "P";
+//                    } else {
+//                        Player.xpos = 0;
+//                        Player.ypos = 0;
+//                        board[0][0] = "P";
+//                        Player.fouls++;
+//                    }
+//                } catch (Exception e) {
+//                    Player.xpos = 0;
+//                    Player.ypos = 0;
+//                    board[0][0] = "P";
+//                    Player.fouls++;
+//                }
+//                board[9][9] = "F";
+//                System.out.print(board[x][i] + "\t");
+//                if (i == 9) {
+//                    System.out.println();
+//                }
+//            }
+//        }
+//    }
+//
+//    public static void playerMoves() {
+//        switch (choice()) {
+//            case 1:
+//                Player.xpos++;
+//                break;
+//            case 2:
+//                Player.xpos--;
+//                break;
+//            case 3:
+//                Player.ypos++;
+//                break;
+//            case 4:
+//                Player.ypos--;
+//                break;
+//        }
+//    }
+//
+//    private static int choice() {
+//        System.out.println("###");
+//        String d = input.nextLine();
+//        if (d.equals("s")) {
+//            return 1;
+//        } else if (d.equals("w")) {
+//            return 2;
+//        } else if (d.equals("d")) {
+//            return 3;
+//        } else if (d.equals("a")) {
+//            return 4;
+//        } else {
+//            return 1;
+//        }
+//    }
+//
+//    public static boolean isGameOver() {
+//        return Player.xpos == 9 && Player.ypos == 9;
+//    }
+//
+//    public static void game() {
+//        int numberOfMoves = 0;
+//        while (!(isGameOver())) {
+//            displayBoard(board);
+//            playerMoves();
+//            numberOfMoves++;
+//        }
+//        int score = lowerOne(100 - numberOfMoves - Player.fouls);
+//        System.out.println("You Won!!!");
+//        input.next();
+//        System.out.println("Your score was " + score + "!");
+//    }
+//
+//    private static int lowerOne(int x) {
+//        if (x < 1) {
+//            return (1);
+//        } else {
+//            return (x);
+//        }
+//    }
--- src/rob/mazeGame/Player.java
+++ src/rob/mazeGame/Player.java
@@ -1,7 +1,101 @@
 package rob.mazeGame;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 public class Player {
     static int xpos = 0;
     static int ypos = 0;
     static int fouls = 0;
+
+    private String player = "rock.gif";
+    private int dx;
+    private int dy;
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private Image image;
+
+    public Player() {
+        ImageIcon i = new ImageIcon(this.getClass().getResource(player));
+        image = i.getImage();
+        x = 168;
+        y = 600;
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+    }
+
+    public void move() {
+        x += dx;
+        y += dy;
+        if (y > 700) {
+            y = 700;
+        } else if (y < 0) {
+            y = 0;
+        }
+        if (x < 0) {
+            x = 0;
+        } else if (x > 362) {
+            x = 362;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -1;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 1;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+
+    public Rectangle getBounds() {
+        return new Rectangle(x, y, width, height);
+    }
 }
