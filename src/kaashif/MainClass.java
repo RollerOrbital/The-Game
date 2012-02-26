e2b4fbcac531058eb80ee010d7fefc792cbaaa75
Kaashif Hymabaccus <sumduud@live.co.uk>
2012-02-26 12:03:39 +0000

    a game where you dodge rocks (rocks don't move yet)

---
+++ src/kaashif/MainClass.java
@@ -0,0 +1,22 @@
+package kaashif;
+
+import javax.swing.JFrame;
+
+public class MainClass extends JFrame {
+
+    public MainClass() {
+
+        add(new Board());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 700);
+        setLocationRelativeTo(null);
+        setTitle("Asteroid Game");
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainClass();
+    }
+}
