61bf62ce57c56ba992a7ede04e7e79b5ef0ab2d1
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-18 14:08:08 +0000

    checking Pierces code

--- src/pierce/MainGame/MainClass.java
+++ src/pierce/MainGame/MainClass.java
@@ -9,7 +9,7 @@ public class MainClass extends JFrame {
         add(new Map());
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize((180*2)+6, (110*2)+28);
+        setSize((180 * 2) + 6, (110 * 2) + 28);
         setLocationRelativeTo(null);
         setTitle("RESIST");
         setResizable(false);
