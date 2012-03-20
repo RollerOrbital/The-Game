a8e92b788e1075d02401b484a298a2ac9d26e9cb
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-20 20:20:39 +0000

    you can only go in lateral directions now

--- src/rob/mainGame/Player.java
+++ src/rob/mainGame/Player.java
@@ -9,6 +9,8 @@ public class Player {
     private int direction, UP, RIGHT, LEFT, DOWN;
     private Image image;
     private int frameNumber;
+    private int[] frameArray;
+    private int isMoving, MOVING, STILL;
 
     public Player() {
         x = 0;
@@ -23,6 +25,13 @@ public class Player {
         ImageIcon playerSheet = new ImageIcon(getClass().getResource("player.png"));
         image = playerSheet.getImage();
         frameNumber = 0;
+        frameArray = new int[4];
+        frameArray[0] = 0;
+        frameArray[1] = 1;
+        frameArray[2] = 0;
+        frameArray[3] = 2;
+        MOVING = 1;
+        STILL = 0;
     }
 
     public int getX() {
@@ -46,28 +55,57 @@ public class Player {
     }
 
     public int getFrame() {
-        return frameNumber;
+        return frameArray[frameNumber % 4] * getWidth();
     }
 
     public int getDirection() {
-        return direction;
+        return direction * getHeight();
+    }
+
+    public void setDirection() {
+        if (dx > 0) {
+            direction = RIGHT;
+        } else if (dx < 0) {
+            direction = LEFT;
+        } else if (dy > 0) {
+            direction = DOWN;
+        } else if (dy < 0) {
+            direction = UP;
+        }
     }
 
     public void move() {
-        x += dx;
-        y += dy;
+        setDirection();
+        if (dy == 0) {
+            x += dx;
+        } else if (dx == 0) {
+            y += dy;
+        }
+        if (isMoving == MOVING) {
+            frameNumber = (frameNumber % 4) + 1;
+        } else if (isMoving == STILL) {
+            frameNumber = 0;
+        }
     }
 
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         if (key == KeyEvent.VK_W) {
             dy = -1;
+            direction = UP;
+            isMoving = MOVING;
         } else if (key == KeyEvent.VK_A) {
             dx = -1;
+            direction = LEFT;
+            isMoving = MOVING;
         } else if (key == KeyEvent.VK_S) {
             dy = 1;
+            direction = DOWN;
+            isMoving = MOVING;
         } else if (key == KeyEvent.VK_D) {
             dx = 1;
+            direction = RIGHT;
+            isMoving = MOVING;
         }
     }
 
@@ -75,12 +113,16 @@ public class Player {
         int key = (e.getKeyCode());
         if (key == KeyEvent.VK_W) {
             dy = 0;
+            isMoving = STILL;
         } else if (key == KeyEvent.VK_A) {
             dx = 0;
+            isMoving = STILL;
         } else if (key == KeyEvent.VK_S) {
             dy = 0;
+            isMoving = STILL;
         } else if (key == KeyEvent.VK_D) {
             dx = 0;
+            isMoving = STILL;
         }
     }
 }
