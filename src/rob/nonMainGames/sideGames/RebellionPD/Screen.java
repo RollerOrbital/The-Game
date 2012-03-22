f4ded26f3c8df87b5306cd36f8a94f45a65231c6
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-22 22:46:32 +0000

    something is amiss

--- src/rob/nonMainGames/sideGames/RebellionPD/Screen.java
+++ src/rob/nonMainGames/sideGames/RebellionPD/Screen.java
@@ -1,12 +1,20 @@
 package rob.nonMainGames.sideGames.RebellionPD;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Screen {
-    private ImageIcon back, fore, choice;
+    private ImageIcon back;
+    private Image backI;
 
     public Screen() {
         //ImageIcons START
+        back = new ImageIcon(getClass().getResource("bg.png"));
+        backI = back.getImage();
         //ImageIcons END
+    }
+
+    public Image getImage() {
+        return backI;
     }
 }
