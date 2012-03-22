729db17b44774e0f12d720806a99851976deb29d
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-22 20:57:01 +0000

    the beginnings of a dungeon crawler

--- src/rob/nonMainGames/sideGames/TextGame/Player.java
+++ src/rob/nonMainGames/sideGames/TextGame/Player.java
@@ -1,9 +1,14 @@
 package rob.nonMainGames.sideGames.TextGame;
 
+import java.util.Scanner;
+
 public class Player {
-    private int x, y;
+    private Scanner input = new Scanner(System.in);
+    private int x, y, dx, dy;
+    private boolean canGo;
 
     public Player() {
+        canGo = true;
         x = 0;
         y = 4;
     }
@@ -14,5 +19,31 @@ public class Player {
 
     public int getY() {
         return x;
+    }
+
+    public void move() {
+        x += dx;
+        y += dy;
+    }
+
+    public void getMove() {
+        if (canGo) {
+            String direction = input.nextLine();
+            if (direction.equals("w")) {
+                dy = -1;
+            } else if (direction.equals("s")) {
+                dy = 1;
+            } else if (direction.equals("a")) {
+                dx = -1;
+            } else if (direction.equals("d")) {
+                dx = 1;
+            }
+            canGo = false;
+        }
+    }
+
+    public void getPlayerTurn() {
+        getMove();
+        move();
     }
 }
