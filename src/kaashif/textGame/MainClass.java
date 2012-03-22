1658798ea5b95cd791f0fc97ce5afd06ddbbbe24
Kaashif Hymabaccus <sumduud@live.co.uk>
2012-03-22 17:54:13 +0000

    a game where you dodge rocks

--- src/kaashif/textGame/MainClass.java
+++ src/kaashif/textGame/MainClass.java
@@ -3,13 +3,12 @@ package kaashif.textGame;
 import javax.swing.*;
 
 public class MainClass extends JFrame{
+
     public MainClass(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(800, 600);
-        setLocationRelativeTo(null);
-        setTitle("Text Game");
-        setResizable(false);
-        setVisible(true);
+        
+        add(new Text());
+    public static void main(String[] args) {
+        new MainClass();
     }
     
 }
