0b94f0c2b949dd413db74c09377f251aeb28b68a
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-23 21:59:14 +0000

    restart on text game --_____--

--- src/rob/nonMainGames/sideGames/TextGame/MainClass.java
+++ src/rob/nonMainGames/sideGames/TextGame/MainClass.java
@@ -1,9 +1,15 @@
 package rob.nonMainGames.sideGames.TextGame;
 
+import java.util.Scanner;
+
 public class MainClass {
 
     public static void main(String[] args) {
-        Map map = new Map();
-        map.getTurn();
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to KettleQuest!");
+        input.next();
+        Player player = new Player();
+        player.name = player.getName();
+        System.out.println("So you are " + player.name + "!");
     }
 }
--- src/rob/nonMainGames/sideGames/TextGame/Player.java
+++ src/rob/nonMainGames/sideGames/TextGame/Player.java
@@ -3,48 +3,14 @@ package rob.nonMainGames.sideGames.TextGame;
 import java.util.Scanner;
 
 public class Player {
+    public String name;
     private Scanner input = new Scanner(System.in);
-    private int x, y, dx, dy;
-    public boolean canGo;
 
     public Player() {
-        canGo = true;
-        x = 0;
-        y = 4;
     }
 
-    public int getX() {
-        return y;
-    }
-
-    public int getY() {
-        return x;
-    }
-
-    public void move() {
-        x += dx;
-        y += dy;
-    }
-
-    public void getMove() {
-        if (canGo) {
-            System.out.println("Your move...");
-            String direction = input.nextLine();
-            if (direction.equals("w")) {
-                dy = -1;
-            } else if (direction.equals("s")) {
-                dy = 1;
-            } else if (direction.equals("a")) {
-                dx = -1;
-            } else if (direction.equals("d")) {
-                dx = 1;
-            }
-            canGo = false;
-        }
-    }
-
-    public void getPlayerTurn() {
-        getMove();
-        move();
+    public String getName() {
+        System.out.println("What is your name?");
+        return input.next();
     }
 }
