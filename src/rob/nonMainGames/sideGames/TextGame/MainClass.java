651ec4f4e48ff515d428d44977758dd44b49afed
Robert Moore <robmoore121@hotmail.co.uk>
2012-03-23 22:13:52 +0000

    update

--- src/rob/nonMainGames/sideGames/TextGame/MainClass.java
+++ src/rob/nonMainGames/sideGames/TextGame/MainClass.java
@@ -1,15 +1,23 @@
 package rob.nonMainGames.sideGames.TextGame;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Random random = new Random();
         System.out.println("Welcome to KettleQuest!");
         input.next();
         Player player = new Player();
         player.name = player.getName();
         System.out.println("So you are " + player.name + "!");
+        input.next();
+        player.region = player.getRegion();
+        System.out.println("So you are from " + player.region + "!");
+        input.next();
+        player.seed = Math.abs((player.seed1 + player.seed2 + random.nextInt()) % 100);
+        System.out.println("Your seed = " + player.seed);
     }
 }
--- src/rob/nonMainGames/sideGames/TextGame/Player.java
+++ src/rob/nonMainGames/sideGames/TextGame/Player.java
@@ -3,7 +3,8 @@ package rob.nonMainGames.sideGames.TextGame;
 import java.util.Scanner;
 
 public class Player {
-    public String name;
+    public String name, region;
+    public int seed1, seed2, seed;
     private Scanner input = new Scanner(System.in);
 
     public Player() {
@@ -11,6 +12,15 @@ public class Player {
 
     public String getName() {
         System.out.println("What is your name?");
-        return input.next();
+        String nam = input.next();
+        seed1 = nam.hashCode();
+        return nam;
+    }
+
+    public String getRegion() {
+        System.out.println("Where are you from?");
+        String reg = input.next();
+        seed2 = reg.hashCode();
+        return reg;
     }
 }
