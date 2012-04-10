08780afbee47f8307f01f049eef4af84beab5bbb
RollerOrbital <joeshmoerobo@hotmail.co.uk>
2012-04-10 15:07:35 +0100

    Updated

---
+++ src/pierce/TerminalVelocity/MainClass.java
@@ -0,0 +1,25 @@
+package pierce.TerminalVelocity;
+
+import javax.swing.*;
+
+public class MainClass extends JFrame {
+
+    public int sq;
+
+    public MainClass() {
+
+        add(new Board());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sq = 32;
+        setSize(13 * sq, 22 * sq);
+        setLocationRelativeTo(null);
+        setTitle("Rock Game");
+        setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainClass();
+    }
+}
